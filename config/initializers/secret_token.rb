17bd1227c1983bbe79d6e60644a16f84b03d000e
James Turnley <turnley.james@gmail.com>
2013-09-15 19:52:30 -0400

    here we go again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Franklin::Application.config.secret_token = 'e3d9ede68062d9d58da24a2c08d6630178b98afc3c5f06d05cdcea5f31917d3533466068a0aec9478a63f4c1ae257966d986e911408cb3da0305ceb0956585f7'
